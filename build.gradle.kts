425c5344d668507e72b07bff65a064970fa175de
Gabriel de Souza Seibel <gabrielseibel1@hotmail.com>
2019-06-17 13:22:58 -0300

    Use coroutines in dibas to send and receive tasks.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-M1")
 }
 
 tasks.withType<KotlinCompile> {
