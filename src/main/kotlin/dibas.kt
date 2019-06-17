c125632df60a2ae6bad00600d65aaec5329ebf83
gabrielseibel1 <gabrielseibel1@gmail.com>
2019-06-17 13:22:58 -0300

    Use coroutines in dibas to send and receive tasks.

--- src/main/kotlin/dibas.kt
+++ src/main/kotlin/dibas.kt
@@ -1,15 +1,77 @@
-fun <T> dibas(cluster: Cluster, block: () -> T): T {
-    println(cluster)
-    //TODO make it run dibas
-    return run(block)
-}
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
+import kotlinx.coroutines.selects.select
 
-fun main() {
-    val cluster = clusterFromFile("./config/cluster.csv")
 
-    val message = dibas(cluster) {
-        "Hello," + " Dibas!"
+data class Task(val request: String, val response: String)
+
+@ExperimentalCoroutinesApi
+suspend fun dibas(produceAndConsume: suspend (todo: SendChannel<Task>, done: ReceiveChannel<Task>) -> Unit) {
+    val todo = Channel<Task>()
+    val done = Channel<Task>()
+
+    //will finish only when the nested launches finish
+    coroutineScope {
+
+        //launch producer and consumer
+        launch {
+            produceAndConsume(todo, done)
+        }
+
+        //launch a coroutine that selects receives on channels
+        launch {
+            while (true) {
+                println("Selecting ...")
+                select<Unit> {
+
+                    //clusterRequest.onReceive { ... }
+
+                    todo.onReceive { task ->
+                        println("Doing $task ...")
+                        done.send(task.copy(response = "done"))
+                    }
+                }
+            }
+        }
     }
 
-    println(message)
+
+    println("Dibas finished")
+}
+
+@ExperimentalCoroutinesApi
+suspend fun produceAndConsume(todo: SendChannel<Task>, done: ReceiveChannel<Task>) {
+
+    //will finish only when the nested launches finish
+    coroutineScope {
+        //launch producer
+        launch {
+            var i = 0
+            while (true) {
+                delay(1000L)
+
+                println("Producing ...")
+                todo.send(Task("Task ${i++}", ""))
+            }
+        }
+
+        //launch consumer
+        launch {
+            println("Consuming ...")
+            done.consumeEach {
+                println("Main received ${it.request}: ${it.response}")
+            }
+        }
+    }
+}
+
+@ExperimentalCoroutinesApi
+fun main() = runBlocking {
+
+    dibas { todo, done ->
+        produceAndConsume(todo, done)
+    }
+
+    //will execute after dibas finishes
+    println("Main finished")
 }
