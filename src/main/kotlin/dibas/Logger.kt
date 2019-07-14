c5a0ee7f96494c376bf0507eb914d24da8f15d33
I508316 <gabriel.seibel@sap.com>
2019-07-14 11:36:43 -0300

    Use custom logger to standardize printing.

---
+++ src/main/kotlin/dibas/Logger.kt
@@ -0,0 +1,26 @@
+package dibas
+
+import java.time.LocalDateTime
+
+interface Logger {
+    fun log(message: String)
+}
+
+class BasicLogger: Logger {
+    override fun log(message: String) {
+        StringBuilder()
+            .append("[${LocalDateTime.now()}] ")
+            .append(message.toLowerCase())
+            .also { println(it.toString()) }
+    }
+}
+
+class ThreadAwareLogger: Logger {
+    override fun log(message: String) {
+        StringBuilder()
+            .append("[${LocalDateTime.now()}] ")
+            .append("[TID ${Thread.currentThread().id}] ")
+            .append(message.toLowerCase())
+            .also { println(it.toString()) }
+    }
+}
